72c1eef47fe40ad504f58342a2ec5add9179ab17
Cozma Rareș <166293428+rarescozma25@users.noreply.github.com>
2025-03-03 10:39:01 +0200

    .

--- ProiectFinal/settings.py
+++ ProiectFinal/settings.py
@@ -63,9 +63,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587 
 EMAIL_USE_TLS = True 
-EMAIL_HOST_USER = 'rares.proiectdjango@gmail.com'
-EMAIL_HOST_PASSWORD = 'byitlnkraglhyras'
-DEFAULT_FROM_EMAIL = 'Bine viata mea te pupa sefu <rares.proiectdjango@gmail.com>'
+
 
 
 ADMINS = [
